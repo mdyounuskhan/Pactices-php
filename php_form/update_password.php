36285276ea641e1e65452b49621d1cd7e3391a92
Younus <cse.younuskhan@gmail.com>
2019-08-13 17:41:16 +0600

    update password field jodi faka thake

--- php_form/update_password.php
+++ php_form/update_password.php
@@ -7,19 +7,21 @@ $re_password = $_POST['re_password'];
 $confiram_password = $_POST['confiram_password'];
 
 $update =  "UPDATE users SET password ='$re_password' WHERE id ='$id'";
-$find = "SELECT COUNT(*) AS pass_info FROM users WHERE email='younus@khan.com' AND password=12345";
 
 
-
-if ($password == $_SESSION['user_password']) {
-    if ($re_password == $confiram_password) {
-        mysqli_query($db_connection, $update);
-        header('location: passwordchange.php');
-        unset($_SESSION['user_password']);
-        $_SESSION['user_password'] = $re_password;
+if ($password && $re_password && $confiram_password) {
+    if ($password == $_SESSION['user_password']) {
+        if ($re_password == $confiram_password) {
+            mysqli_query($db_connection, $update);
+            header('location: passwordchange.php');
+            unset($_SESSION['user_password']);
+            $_SESSION['user_password'] = $re_password;
+        } else {
+            echo "porer 2 ta mile nai";
+        }
     } else {
-        echo "porer 2 ta mile nai";
+        echo "first pass vul";
     }
 } else {
-    echo "first pass vul";
+    echo "Your Input Empty!";
 }
